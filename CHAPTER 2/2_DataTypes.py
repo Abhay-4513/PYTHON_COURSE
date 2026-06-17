ccb5eea940115f50d5cff84cb9ad674ef5a0f578
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-06-17 08:52:50 +0530

    Add files via upload

---
+++ CHAPTER 2/2_DataTypes.py
@@ -0,0 +1,9 @@
+a = 5 # a is an integer
+
+b = 5.22 # b is a floating number 
+
+c = True # c is a Boolean type (True/False) 
+
+d = "Abhay"  # d is a string ("String is also called as the collection of characters")
+
+e = None # e is a none type variable
