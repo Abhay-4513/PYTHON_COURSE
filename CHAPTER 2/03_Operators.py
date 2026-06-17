ed396c1b7a61d87b9228ad22f39689a269f8928d
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-06-17 08:53:27 +0530

    Create 03_Operators.py

---
+++ CHAPTER 2/03_Operators.py
@@ -0,0 +1,57 @@
+# Arithmetic operators
+
+a = 5
+b = 6
+c = a + b
+print(c)
+
+# Assignment operators
+
+d = 5 - 3
+print(d)
+
+f = 4
+f+=3  # This means increase the value of f by 3 (Increment )
+print(f)
+f-=2 # This means decrease the value of f by 2 (Decrement)
+print(f)
+
+
+# Comparison operaators
+
+g = 5<2
+print(g)
+
+h = 5>2
+print(h)
+
+i = 5!=5
+print(i)
+
+j = 5==5
+print(j)
+
+# Logical operators
+
+k = True or False
+print(k)
+print("=============================================")
+# OR truth table 
+
+print(f"True or Flase is : {True or False}")
+print(f"False or True is : {False or True }")
+print(f"True or True is : {True or True}")
+print(f"False or False is : {False or False}")
+print("=============================================")
+# AND truth table
+
+print(f"True or Flase is : {True and False}")
+print(f"False or True is : {False and True }")
+print(f"True or True is : {True and True}")
+print(f"False or False is : {False and False}")
+print("=============================================")
+
+# NOT operator 
+
+print(not(False))
+print(not(True))
