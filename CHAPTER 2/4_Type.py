46b5abff57f665a8c8a79be6f9bea15c4886c199
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-06-17 08:54:19 +0530

    Create 4_Type.py

---
+++ CHAPTER 2/4_Type.py
@@ -0,0 +1,17 @@
+# Type() Function AND Type Casting
+
+a = 5
+print(type(a))
+
+b = 34.0
+print(type(b))
+
+c = "Abhay"
+print(type(c))
+
+# How to change the type 
+
+d = "54"
+e = int(d)
+print(e)
+print(type(e))
