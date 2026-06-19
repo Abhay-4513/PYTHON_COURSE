ff33cfafd510fc45355666fd0d4f7035810ddde1
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-06-19 19:03:34 +0530

    Create Problem_1.py

---
+++ CHAPTER 2/PRACTICE SET/Problem_1.py
@@ -0,0 +1,8 @@
+# Write a python program to add two numbers.....
+
+a = int(input("Enter the first number : ")) # User 1st number
+b = int(input("Enter the second number : ")) # User 2nd number
+
+c = a+b  # Variable 'c' is used to store the addition ...
+
+print(f"The Addition of above 2 numbers is : {c}")
