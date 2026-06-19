6f4d4e18871eb483bdfe13a02b52cbca914bc70f
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-06-19 19:12:28 +0530

    Create Problem_2.py

---
+++ CHAPTER 2/PRACTICE SET/Problem_2.py
@@ -0,0 +1,9 @@
+# Write a python program to find the remainder when a number is divided by z
+
+a = float(input("Enter the Dividend : "))
+b = float(input("Enter the divisor : "))
+
+c = (a/b)
+print(c)
+r = (a%b)
+print(r) 
