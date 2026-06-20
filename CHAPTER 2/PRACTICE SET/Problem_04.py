aa427de5a25321b0c8595fe4291a36e4648fc35b
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-06-20 13:29:31 +0530

    Add files via upload

---
+++ CHAPTER 2/PRACTICE SET/Problem_04.py
@@ -0,0 +1,6 @@
+# Use comparison operator to find out whether a given variable a is greater than b or not 
+
+a = int(input("Enter a number : "))
+b = int(input("Enter another number : "))
+
+print(f"Is 'a' greater than 'b' : {a>b}")
