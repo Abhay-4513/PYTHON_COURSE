9d2c8f2f90db0812115a2f223058f5386168fc86
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-06-24 08:39:17 +0530

    Add files via upload

---
+++ CHAPTER 3/01_String.py
@@ -0,0 +1,14 @@
+# String is a collection of characters , which is represented in quotes('single quote '. "double quote ", '''triple quote ''')
+# If we start from first place indexing will start from '0' and is we start from the last place it will start from '-1'
+a = "Abhay"
+
+b = 'Best'
+
+c = '''Programmer'''
+
+nameshort = a[0:3]  # Start from index 0 to all the way till 3 (excluding 3)  , 
+
+print(nameshort)
+
+character1 = a[1]
+print(character1)
