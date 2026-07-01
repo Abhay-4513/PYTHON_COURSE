799126eb4f773ce534abb4d5301682063b44f35c
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-01 16:59:06 +0530

    Create 03_String_functions.py

---
+++ CHAPTER 3/03_String_functions.py
@@ -0,0 +1,8 @@
+name ="avengers"
+
+print(len(name))
+print(name.endswith("gers"))  # This give the true/false value
+print(name.startswith("Aven")) # This give the true/false value
+print(name.capitalize())   # This function only capitalize the first letter......
+print(name.count("e"))  # This will count the number of 'specific letter' in the string 
+print(name.find("n"))  # This will give the index of the letter which we want to search .
