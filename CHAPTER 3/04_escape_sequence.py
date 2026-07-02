91be42fc405450b50af8bba99c3a31c007e7811c
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-02 09:44:28 +0530

    Add files via upload

---
+++ CHAPTER 3/04_escape_sequence.py
@@ -0,0 +1,11 @@
+#""
+# \n : newline
+# \t : tab
+# \; : singlequote
+# \  : backslash
+# \t : Horizontal tab
+# \xhh : Character with hexadecimal value
+# ""
+a = "Abhay is the best programmer \nHe is so \"talented\""
+
+print(a)
