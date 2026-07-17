6a8498d15f459ad960dd9f87139632391d7d943c
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-17 08:50:50 +0530

    Add files via upload

---
+++ CHAPTER 4/03_Tuple.py
@@ -0,0 +1,8 @@
+a = () # This is an empty tuple
+print(type(a))
+
+b = (1,) # Tuple with single element
+print(type(b))
+
+c = (1,2,3,"Abhay","New","Code") # Tuple is the combination of String and List , it is immutable (cannot change it) but you can store different data types....
+print(type(c))
