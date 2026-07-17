e382a143aefac022313989c903eaa1864fabed3b
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-17 08:51:06 +0530

    Add files via upload

---
+++ CHAPTER 4/04_Tuple_methods.py
@@ -0,0 +1,9 @@
+a = (1,2,3,"Lion","Monkey","Money",24,25)
+
+COUNT = a.count(25) # This is used to count the specific element appearance
+print(COUNT)
+
+INDEX = a.index("Lion") # This is used to print the position of an element
+print(INDEX)
+
+print(len(a))
