747fa93cbcf0377642b1e81bc8377a2271a42ecf
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-18 15:13:52 +0530

    Update Problem_02.py

--- CHAPTER 4/PRACTICE SET/Problem_02.py
+++ CHAPTER 4/PRACTICE SET/Problem_02.py
@@ -1,9 +1,23 @@
 # Write a program to accept marks of 6 students and display them in sorted order
 
 marks = []
+studnet_1 = int(input(f"Enter the mark of student : "))
+marks.append(studnet_1)
 
-for i in range(1,7):
-    studnet = int(input(f"Enter the mark of student {i} : "))
-    marks.append(studnet)
-    marks.sort()
+studnet_2 = int(input(f"Enter the mark of student : "))
+marks.append(studnet_2)
+
+studnet_3 = int(input(f"Enter the mark of student : "))
+marks.append(studnet_3)
+
+studnet_4 = int(input(f"Enter the mark of student : "))
+marks.append(studnet_4)
+
+studnet_5 = int(input(f"Enter the mark of student : "))
+marks.append(studnet_5)
+
+studnet_6 = int(input(f"Enter the mark of student : "))
+marks.append(studnet_6)
+
+marks.sort()
 print(marks)
