f8fe2226b695993532ccfcfb1a6c0421852f734e
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-18 15:13:22 +0530

    Update Problem_01.py

--- CHAPTER 4/PRACTICE SET/Problem_01.py
+++ CHAPTER 4/PRACTICE SET/Problem_01.py
@@ -2,25 +2,25 @@
 
 fruits = []
 
-# fruit_1 = input("Enter 1st fruit name : ")
-# fruits.append(fruit_1)
+fruit_1 = input("Enter 1st fruit name : ")
+fruits.append(fruit_1)
 
-# fruit_2 = input("Enter 2nd fruit name : ")
-# fruits.append(fruit_2)
+fruit_2 = input("Enter 2nd fruit name : ")
+fruits.append(fruit_2)
 
-# fruit_3 = input("Enter 3rd fruit name : ")
-# fruits.append(fruit_3)
+fruit_3 = input("Enter 3rd fruit name : ")
+fruits.append(fruit_3)
 
-# fruit_4 = input("Enter 4th fruit name : ")
-# fruits.append(fruit_4)
+fruit_4 = input("Enter 4th fruit name : ")
+fruits.append(fruit_4)
 
-# fruit_5 = input("Enter 5th fruit name : ")
-# fruits.append(fruit_5)
+fruit_5 = input("Enter 5th fruit name : ")
+fruits.append(fruit_5)
 
-# fruit_6 = input("Enter 6th fruit name : ")
-# fruits.append(fruit_6)
+fruit_6 = input("Enter 6th fruit name : ")
+fruits.append(fruit_6)
 
-# fruit_7 = input("Enter 7th fruit name : ")
-# fruits.append(fruit_7)
+fruit_7 = input("Enter 7th fruit name : ")
+fruits.append(fruit_7)
 
-# print(fruits)
+print(fruits)
