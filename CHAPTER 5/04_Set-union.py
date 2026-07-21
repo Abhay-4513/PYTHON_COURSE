10344fa2f71d3c631e2630274866a06873927007
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-21 20:15:40 +0530

    Add files via upload

---
+++ CHAPTER 5/04_Set-union.py
@@ -0,0 +1,7 @@
+s1 = {1,3,5,7}
+s2 = {1,2,4,5,6}
+
+Union = s1.union(s2) # This will give the value from both sets , without repeating the values
+print(Union)
+ 
+print(s2.intersection(s1)) # This will give the common value from both the sets.....
