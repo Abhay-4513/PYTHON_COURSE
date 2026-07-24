7af2308f28fc4210825ce03990d090c5a529c2e4
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-24 19:29:52 +0530

    Create 01_Conditional.py

---
+++ CHAPTER 6/01_Conditional.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age : "))
+
+if (age>=18):
+    print("You are eligible for voting")
+    print("yes")
+
+else:
+    print("Your are not eligible for voting")
+    print("No")
