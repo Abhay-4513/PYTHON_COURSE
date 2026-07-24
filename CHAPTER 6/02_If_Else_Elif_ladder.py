ae863a1370e2f1122277cd6adfc6a3fcef8526d5
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-24 19:31:30 +0530

    Create 02_If_Else_Elif_ladder.py

---
+++ CHAPTER 6/02_If_Else_Elif_ladder.py
@@ -0,0 +1,20 @@
+num = int(input("Enter your number : "))
+
+if (num>=90):
+    print("Excellent work !")
+
+elif (num>=80):
+    print("Very good.")
+
+elif (num>=70):
+    print("Good..")
+
+elif (num>=60):
+    print("Try better next time -_-")
+    
+elif (num>=50):
+    print("You will become a politician..(in india)")
+
+else:
+    print("Congraluations .... Your are failed")
+    
