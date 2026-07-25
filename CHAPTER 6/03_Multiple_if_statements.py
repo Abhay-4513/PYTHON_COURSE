161ad8785b053e5a765ed6651b61acf2f3857eda
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-07-25 10:50:21 +0530

    Add files via upload

---
+++ CHAPTER 6/03_Multiple_if_statements.py
@@ -0,0 +1,18 @@
+a = int(input("Enter you age : "))
+
+#-----if statement no.1 start here
+if (a%2==0):
+    print("It is a even number..")
+else:
+    print("It is a odd number .")
+#-----if statement no.1 end here
+
+#-----if statement no.2 start here
+if (a>=18):
+    print("You are eligible for voting")
+    print("yes")
+
+else:
+    print("Your are not eligible for voting")
+    print("No")
+#-----if statement no.2 end here
