0b9108cf560591a04707ba9b70a78f6f1666e6b2
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-08-07 14:18:57 +0530

    Update basic-math.py

--- Practice/basic-math.py
+++ Practice/basic-math.py
@@ -1,9 +1,11 @@
+# Area of rectangle....
 rec_width = float(input("Enter the value of width : "))
 rec_breadth = float(input("Enter the value of breadth : "))
 
 rec_area = width*breadth 
 print(area)
 
+# Area of square....
 sq_side_1 = int(input("Enter the first side value : "))
 sq_side_2 = int(input("Enter the second side : "))
 sq_area = sq_side_1 * sq_side_2   # you can also use a**
