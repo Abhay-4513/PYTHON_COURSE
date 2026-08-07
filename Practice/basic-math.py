3fceea14f2095f398b8b424530565f2aecf36f4e
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-08-07 14:17:59 +0530

    Create basic-math.py

---
+++ Practice/basic-math.py
@@ -0,0 +1,10 @@
+rec_width = float(input("Enter the value of width : "))
+rec_breadth = float(input("Enter the value of breadth : "))
+
+rec_area = width*breadth 
+print(area)
+
+sq_side_1 = int(input("Enter the first side value : "))
+sq_side_2 = int(input("Enter the second side : "))
+sq_area = sq_side_1 * sq_side_2   # you can also use a**
+print(sq_area)
