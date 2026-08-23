d3989850a10b95aa8db19a4bbaffe7e59353ac61
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-08-23 21:45:18 +0530

    Modify divisor program to collect and display divisors
    Store divisors in a list and print them along with their count.

--- Practice/divisor-factor.py
+++ Practice/divisor-factor.py
@@ -1,6 +1,9 @@
 # This program will give the factors which are perfect divisors of the given number.
 n = int(input("Enter the number : "))
-
+a = []
 for i in range(1,n+1):
   if n%i==0:
-    print(i)
+    a.append(i)
+
+print(a)
+print(f"Number of divisors: {len(a)}")
