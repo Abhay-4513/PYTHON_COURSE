3d9fc0df5dee240d31aa99a0f980e8b052324614
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-08-24 22:13:55 +0530

    Refactor n-square series sum program
    Fix indentation and improve output formatting.

--- Practice/n-square-series-sum.py
+++ Practice/n-square-series-sum.py
@@ -1,5 +1,10 @@
 #This program will the output of this series : 1^2 + 2^2 + 3^2 + ... + n^2.
 n = int(input("Enter the number : "))
-
+a = 0
+c = []
 for i in range(1,n+1):
-  print(f"Square of {i} is = {i*i}")
+  b = i*i
+  a = a + b
+  c.append(b)
+print("The series is : ", c)
+print("The sum of the series is : ", a)
