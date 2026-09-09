0c2920c28a26d04f4279c5cf5c33021dd6d385bb
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-09-09 23:32:46 +0530

    continue function added

--- CHAPTER 7/07_break_continue.py
+++ CHAPTER 7/07_break_continue.py
@@ -2,3 +2,8 @@ for i in range(1,11):
     if i ==7:
         break # exits the loop when it reaches 7.
     print(i)
+
+for j in range(100):
+    if (j==34):
+        continue # skips the rest of the code in the loop when it reaches 34.
+    print(j)
