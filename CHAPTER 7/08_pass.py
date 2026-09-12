39e99d9ad0671c7976ffd0ef6cd75f02322b0f1b
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-09-12 22:26:42 +0530

    pass function

---
+++ CHAPTER 7/08_pass.py
@@ -0,0 +1,7 @@
+for i in range(1,101):
+    pass              # Without pass the program will throw an error because the for loop is empty. The pass statement is used as a placeholder to indicate that no action is to be taken.
+
+i = 0
+while i < 100:
+    print(i)
+    i += 1
