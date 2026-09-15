d509c0e5ab67ede79247b4a5d7d508d76fbc08ee
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-09-15 18:53:16 +0530

    Problem 8 solved

---
+++ CHAPTER 7/PRACTICE SET/08.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 18:52:37 2026
+
+@author: abhay
+"""
+
+# Write a program to print the following star pattern 
+#*
+#**
+#***
+
+
+n = int(input("Enter a number : "))
+
+for i in range(1,n+1):
+    print("*"*(i))
