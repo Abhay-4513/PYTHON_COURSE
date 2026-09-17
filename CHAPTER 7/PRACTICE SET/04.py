a5ca8e15717d4ceff2a7abf94f3d10c5db429baf
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-09-17 14:38:34 +0530

    problem 4 resolved

--- CHAPTER 7/PRACTICE SET/04.py
+++ CHAPTER 7/PRACTICE SET/04.py
@@ -1,5 +1,8 @@
 n = int(input("Enter the number : "))
 
-for i in range(n):
+for i in range(2,n):
     if n % i == 0:
-        print
+        print("Not a prime number")
+        break
+else:
+    print("Prime number")
