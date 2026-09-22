1067b1432ea8b5eec9d6d8c81922201532831c2a
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-09-22 19:39:48 +0530

    -

--- CHAPTER 8/PRACTICE SET/02.py
+++ CHAPTER 8/PRACTICE SET/02.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Write a program using a function to convert temperature in Celsius to Fahrenheit. The formula is F = (C * 9/5) + 32
 """
 Created on Tue Sep 22 17:58:49 2026
 
@@ -8,5 +9,6 @@ Created on Tue Sep 22 17:58:49 2026
 def celsiusFahrenheit(c):
     f = (c*(9/5))+32
     print(f"The temperature in fahrenheit is : {f}")
+
 c = float(input("Enter the temperature in celsius : "))
 celsiusFahrenheit(c)
