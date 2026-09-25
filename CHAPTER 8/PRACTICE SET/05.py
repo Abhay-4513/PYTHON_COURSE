c4ca3a7f965aa269a85d720846cd9db8ecb1b640
Abhay Tripathi <abhaytripathi270@gmail.com>
2026-09-25 12:00:00 +0530

    problem 5 solved

---
+++ CHAPTER 8/PRACTICE SET/05.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 22:24:22 2026
+
+@author: abhay
+"""
+
+def pattern(n):
+    
+    if n == 0:
+        return 
+    
+    print("*"*n)
+    pattern(n-1)
+
+n = int(input("Enter a number : "))
+pattern(n)
